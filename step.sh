c74a7a55e73aa00a6468a351deed5648e271bc50
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-11-03 07:34:14 +0900

    add head_sha parameter

--- step.sh
+++ step.sh
@@ -5,6 +5,6 @@ INPUT_PATH="$xcresult_path" \
 INPUT_TITLE="$title" \
 INPUT_GITHUB_OWNER="$github_owner" \
 INPUT_GITHUB_REPO="$github_repo" \
-INPUT_GITHUB_SHA="${GIT_CLONE_COMMIT_HASH:-$BITRISE_GIT_COMMIT}" \
+INPUT_GITHUB_SHA="${head_sha:-$BITRISE_GIT_COMMIT}" \
 INPUT_TOKEN="$(uuidgen)" \
  node --unhandled-rejections=strict "$BITRISE_STEP_SOURCE_DIR/dist/index.js"
